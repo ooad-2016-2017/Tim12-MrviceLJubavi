7117027dac8760f75d20ac98760fd48f9abad715
Elma Kupusovic <ekupusovic1@etf.unsa.ba>
2017-05-26 19:29:27 +0200

    Dodan xaml za dodavanje novosti, prepravljeni xamlovi (GlavnaStranica, Informacije, App)

--- GlasajBa/GlasajBa/App.xaml.cs
+++ GlasajBa/GlasajBa/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using GlasajBa.View;
 
 namespace GlasajBa
 {
@@ -72,7 +73,7 @@ namespace GlasajBa
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(GlavnaStranica), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
