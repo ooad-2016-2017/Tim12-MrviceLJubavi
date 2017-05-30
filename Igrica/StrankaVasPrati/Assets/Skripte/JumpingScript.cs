abae04aa19f76aa4382abe0df6eb4b167f75159c
Belma Muftic <bmuftic1@etf.unsa.ba>
2017-05-30 21:13:38 +0200

    Dodane dvije skripte za igricu (skakanje i hodanje), kao i tlo, listici, i pozadina.

---
+++ Igrica/StrankaVasPrati/Assets/Skripte/JumpingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingScript : MonoBehaviour {
+
+
+
+    public Rigidbody2D stickman;
+
+	// Use this for initialization
+	void Start () {
+        //treba instancirati stickmana
+        stickman = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            stickman.velocity = new Vector2(1,(float)4.5);
+        }
+	}
+
+    //nedovrseno, treba stickman
+    public bool stojiLi()
+    {
+        if (stickman.velocity.y<-4.1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Igrica/StrankaVasPrati/Assets/Skripte/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody2D stickman;
+    static float povecaj=(float)1.01;
+
+    //[SerializeField]
+    //private float brzina;
+
+	void Start () {
+        stickman = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        HandleMovement();
+	}
+
+    void HandleMovement()
+    {
+        stickman.velocity = new Vector2(1*povecaj, 0);
+        povecaj = stickman.velocity.x;
+    }
+}
