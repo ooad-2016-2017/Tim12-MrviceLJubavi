9ef78efe75c352cc7f5039574bb6d8f7546c515a
Ehlimana Krupalija <ekrupalija1@etf.unsa.ba>
2017-05-31 18:24:57 +0200

    Dodana GameOver scena i povezane kontrole.

---
+++ Igrica/StrankaVasPrati/Assets/Skripte/GameOverScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void izlaz()
+    {
+        Application.Quit();
+    }
+    public void Ponovi()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Igrica/StrankaVasPrati/Assets/Skripte/MainMenuScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    int kojiIgrac = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void pokreniIgru1 ()
+    {
+        SceneManager.LoadScene("StrankaVasPrati");
+        kojiIgrac = 1;
+        
+    }
+    public void pokreniIgru2 ()
+    {
+        SceneManager.LoadScene("StrankaVasPrati");
+        kojiIgrac = 2;
+    }
+    public void pokreniIgru3 ()
+    {
+        SceneManager.LoadScene("StrankaVasPrati");
+        kojiIgrac = 3;
+    }
+    public void pokreniIgru4 ()
+    {
+        SceneManager.LoadScene("StrankaVasPrati");
+        kojiIgrac = 4;
+    }
+    public void izlaz ()
+    {
+        Application.Quit();
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        GameObject[] objekti = SceneManager.GetSceneByName("StrankaVasPrati").GetRootGameObjects();
+        foreach (GameObject g in objekti)
+        {
+            if (kojiIgrac == 1)
+            {
+                if (g.name == "AE") g.transform.localScale = new Vector3(1, 1, 1);
+                else if (g.name == "EE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "RI") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "TK") g.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else if (kojiIgrac==2)
+            {
+                if (g.name == "AE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "EE") g.transform.localScale = new Vector3(1, 1, 1);
+                else if (g.name == "RI") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "TK") g.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else if (kojiIgrac == 3)
+            {
+                if (g.name == "AE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "EE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "RI") g.transform.localScale = new Vector3(1, 1, 1);
+                else if (g.name == "TK") g.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else if (kojiIgrac == 4)
+            {
+                if (g.name == "AE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "EE") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "RI") g.transform.localScale = new Vector3(0, 0, 0);
+                else if (g.name == "TK") g.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+}
