677f73f575aa45a46981d15846a3676342dc6e60
Belma Muftic <bmuftic1@etf.unsa.ba>
2017-05-31 10:21:22 +0200

    Dodane skripte za kreiranje i ponasanje objekta Listic.

---
+++ Igrica/StrankaVasPrati/Assets/Skripte/ListicBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListicBehavior : MonoBehaviour {
+
+    // Use this for initialization
+    public float brzina = 1;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position - new Vector3(brzina * Time.deltaTime, 0,0);
+	}
+
+    void onCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Stickman")
+        {
+            Destroy(gameObject);
+            //provjeriti da li je zlatni listic ili obicni i povecati score ovdje
+        }
+    }
+}
---
+++ Igrica/StrankaVasPrati/Assets/Skripte/ListicCreator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListicCreator : MonoBehaviour {
+
+    public GameObject[] mete;
+    private float cooldown = -2f;
+    public float cooldownAmount = 2;
+    public Material[] materijal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cooldown < 0)
+        {
+            createMeta();
+            cooldown = cooldownAmount;
+        }
+        cooldown -= Time.deltaTime;
+    }
+    ListicFactory factory = new ListicFactory();
+    private void createMeta()
+    {
+        //na random y poziciji
+        Vector2 pozicija = new Vector2(10, (UnityEngine.Random.Range(-2.3f, 3.4f)));
+        //Instantiate(factory.getLisic("Zlatni"), pozicija, transform.rotation);
+    }
+
+    private class ListicFactory
+    {
+        Dictionary<String, GameObject> listici = new Dictionary<string, GameObject>();
+        public GameObject getLisic(String boja) {
+            if (listici.ContainsKey(boja))
+            {
+                return listici[boja];
+            } else
+            {
+                
+                listici.Add(boja, new GameObject()); //provjeri kako se tacno kreira instanca objekta sa nekom bojom, tj Material
+                return listici[boja];
+            }
+        }
+    }
+}
